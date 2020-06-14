9e8bd8965df465d1a5bd7cfbbb5eeb2035a3cf9c
understarsky <13349204335@qq.com>
2020-06-15 01:17:14 +0800

    20. 有效的括号

---
+++ src/leetcode/problem/simple/IsValid.java
@@ -0,0 +1,29 @@
+package leetcode.problem.simple;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+class IsValid {
+    private HashMap<Character, Character> mappings;
+    public IsValid() {
+        this.mappings = new HashMap<Character, Character>();
+        this.mappings.put(')', '(');
+        this.mappings.put('}', '{');
+        this.mappings.put(']', '[');
+    }
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (this.mappings.containsKey(c)) {
+                char topElement = stack.empty() ? '#' : stack.pop();
+                if (topElement != this.mappings.get(c)) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
