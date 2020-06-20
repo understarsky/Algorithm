419d276b51cdb8559e949642998dade43cd6e9b8
understarsky <13349204335@qq.com>
2020-06-21 01:03:47 +0800

    58. 最后一个单词的长度

--- src/leetcode/problem/simple/LengthOfLastWord.java
+++ src/leetcode/problem/simple/LengthOfLastWord.java
@@ -1,5 +1,5 @@
 package leetcode.problem.simple;
-
+//58. 最后一个单词的长度  40% split分割，获取
 public class LengthOfLastWord {
     public int lengthOfLastWord(String s) {
         String[] ada = s.split(" ");
